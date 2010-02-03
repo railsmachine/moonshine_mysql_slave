edd1c52fde789616ce6729a83e8345b8ac66eb75
Ches Martin <ches@whiskeyandgrits.net>
2010-02-03 21:26:28 +0700

    Whoops, add a cron interval to the periodic backup cleanup

--- lib/mysql_slave.rb
+++ lib/mysql_slave.rb
@@ -87,7 +87,8 @@ EOF
       :command => "/usr/bin/innobackupex-1.5.1 --defaults-file=#{options[:defaults_file] || '/etc/mysql/my.cnf'} --stream=tar #{target_dir} | /bin/gzip - > #{target_dir}/xtrabackup_`hostname`_`date +%Y%m%d-%H%M%S`.tar.gz >> #{configuration[:deploy_to]}/current/log/cron.log 2>> #{configuration[:deploy_to]}/current/cron.error.log",
       :hour => "*/#{options[:hour_interval] || 4}"
     cron 'xtrabackup-prune',
-      :command => "cd #{target_dir} && rm $(ls -t #{target_dir} | awk 'NR > #{options[:retain] || 1} { print $1 }') >> #{configuration[:deploy_to]}/current/log/cron.log 2>> #{configuration[:deploy_to]}/current/cron.error.log"
+      :command => "cd #{target_dir} && rm $(ls -t #{target_dir} | awk 'NR > #{options[:retain] || 1} { print $1 }') >> #{configuration[:deploy_to]}/current/log/cron.log 2>> #{configuration[:deploy_to]}/current/cron.error.log",
+      :hour => "*/#{options[:hour_interval] || 4}"
   end
 
 end
