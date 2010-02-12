3232b19e6eb05f5eb35d060480e303a336a8f15f
Ches Martin <ches@whiskeyandgrits.net>
2010-02-12 20:27:15 +0700

    one more dumb cron fix
    Yikes, don't run *every minute* of every 4th hours...

--- lib/mysql_slave.rb
+++ lib/mysql_slave.rb
@@ -87,10 +87,12 @@ EOF
     cron 'xtrabackup-snapshots',
       # Why does this package install only a version-numbered binary? Argh.
       :command => "/usr/bin/innobackupex-1.5.1 --defaults-file=#{options[:defaults_file] || '/etc/mysql/my.cnf'} --stream=tar #{target_dir} | /bin/gzip - > #{target_dir}/xtrabackup_`hostname`_`date '+\\%Y\\%m\\%d-\\%H\\%M\\%S'`.tar.gz 2>> #{configuration[:deploy_to]}/current/log/cron.error.log",
-      :hour => "*/#{options[:hour_interval] || 4}"
+      :hour => "*/#{options[:hour_interval] || 4}",
+      :minute => '0'
     cron 'xtrabackup-prune',
       :command => "cd #{target_dir} && rm $(ls -t #{target_dir} | awk 'NR > #{options[:retain] || 1} { print $1 }') >> #{configuration[:deploy_to]}/current/log/cron.log 2>> #{configuration[:deploy_to]}/current/log/cron.error.log",
-      :hour => "*/#{options[:hour_interval] || 4}"
+      :hour => "*/#{options[:hour_interval] || 4}",
+      :minute => '8'
   end
 
 end
